c9796f5271e374f70bd1195795b5c80e00e11de1
robossut <romain.bossut@gmx.com>
2024-05-30 10:21:25 +0200

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <p>Home</p>
+  )
+}
+
+export default Home
